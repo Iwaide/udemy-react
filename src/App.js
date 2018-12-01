54e47d1c8a4545e2d8ee1517abac4cbc34277f7b
Shinnosuke Iwaide <iwaide@hoobby.net>
2018-12-01 13:37:28 +0900

    functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onKeyUp={()=> {console.log('aaa')}} / >
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onKeyUp={()=> {console.log('aaa')}} / >
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+const Cat = () => {
+  return <div>Meow</div>;
 }
 
 export default App;
